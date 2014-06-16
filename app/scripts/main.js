abddad204f68f303a8d04ba981852ee9c5a202bf
Iain Andrew <iainbruceandrew@gmail.com>
2014-06-16 15:18:33 +0100

    added scroll rotate functionality to tape machine

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+var sdegree = 0;
+
+$(window).scroll(function(event){
+	var st = $(this).scrollTop(),
+		rotate = "rotate(" + st + "deg)";
+
+	$(".tm-reel-left").css({transform : rotate});
+	$(".tm-reel-right").css({transform : rotate});
+
+});
