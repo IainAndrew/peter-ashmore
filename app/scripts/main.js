39997d6672b5ffbfee58dbfe9a07aff52c504db7
Iain Andrew <iainbruceandrew@gmail.com>
2014-06-17 22:57:19 +0100

    added navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,13 +6,27 @@ $(window).scroll(function(){
 
 	$('.tm-reel-left').css({transform : rotate});
 	$('.tm-reel-right').css({transform : rotate});
-	$('.tm-meters').css({
-		background: '#F7F374'
-	});
+	$('.tm-meters').addClass('tm-meters-light');
 	clearTimeout($.data(this, 'scrollTimer'));
 	$.data(this, 'scrollTimer', setTimeout(function() {
-		$('.tm-meters').css({
-			background: 'grey'
-		});
+		$('.tm-meters').removeClass('tm-meters-light');
 	}, 200));
+});
+
+$(function() {
+	var toggle = $('#nav-toggle'),
+		menu = $('nav ul'),
+		icon = $('#nav-toggle i');
+
+	toggle.click(function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
+	$(window).resize(function() {
+		var width = $(window).width();
+		if (width > 320 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
 });
