81ef53ad1cde43c1d238ed914a34e07379d33868
Iain Andrew <iainbruceandrew@gmail.com>
2014-06-17 13:42:14 +0100

    added tape machine meter animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-//'use strict';
+'use strict';
 
 
 var sdegree = 0;
@@ -7,7 +7,19 @@ $(window).scroll(function(event){
 	var st = $(this).scrollTop(),
 		rotate = "rotate(" + st + "deg)";
 
-	$(".tm-reel-left").css({transform : rotate});
-	$(".tm-reel-right").css({transform : rotate});
+	$('.tm-reel-left').css({transform : rotate});
+	$('.tm-reel-right').css({transform : rotate});
+	$('.tm-meters').css({
+    	background: '#F7F374'
+    });
+
+	$(window).scroll(function() {
+	    clearTimeout($.data(this, 'scrollTimer'));
+	    $.data(this, 'scrollTimer', setTimeout(function() {
+	        $('.tm-meters').css({
+	        	background: 'grey'
+	        });
+	    }, 200));
+	});
 
 });
